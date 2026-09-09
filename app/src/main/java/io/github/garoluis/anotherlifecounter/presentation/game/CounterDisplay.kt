71461632e2de807895d34e26524c1a5ed33d39a6
GaroLuis <garoluis94@gmail.com>
2026-09-09 14:52:30 +0200

    feat: add repeatingClickable modifier

--- app/src/main/java/io/github/garoluis/anotherlifecounter/presentation/game/CounterDisplay.kt
+++ app/src/main/java/io/github/garoluis/anotherlifecounter/presentation/game/CounterDisplay.kt
@@ -1,5 +1,11 @@
 package io.github.garoluis.anotherlifecounter.presentation.game
 
+import android.util.Log
+import androidx.compose.foundation.combinedClickable
+import androidx.compose.foundation.gestures.awaitEachGesture
+import androidx.compose.foundation.gestures.awaitFirstDown
+import androidx.compose.foundation.gestures.waitForUpOrCancellation
+import androidx.compose.foundation.interaction.InteractionSource
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
@@ -11,13 +17,21 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.rememberUpdatedState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.composed
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.coerceAtLeast
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlin.time.Duration.Companion.milliseconds
 
 @Composable
 fun CounterDisplay(
@@ -38,8 +52,12 @@ fun CounterDisplay(
         horizontalArrangement = Arrangement.SpaceEvenly
     ) {
         TextButton(
-            onClick = onDecrement,
-            modifier = Modifier.size((if (isLarge) 56.dp else 36.dp).scaled(scale).coerceAtLeast(32.dp)),
+            onClick = {},
+            modifier = Modifier
+                .size((if (isLarge) 56.dp else 36.dp).scaled(scale).coerceAtLeast(32.dp))
+                .repeatingClickable(
+                    onClick = onDecrement,
+                ),
             contentPadding = if (isLarge) ButtonDefaults.TextButtonContentPadding else PaddingValues(0.dp),
             colors = ButtonDefaults.textButtonColors(
                 contentColor = decrementColor
@@ -84,8 +102,12 @@ fun CounterDisplay(
         )
 
         TextButton(
-            onClick = onIncrement,
-            modifier = Modifier.size((if (isLarge) 56.dp else 36.dp).scaled(scale).coerceAtLeast(32.dp)),
+            onClick = {},
+            modifier = Modifier
+                .size((if (isLarge) 56.dp else 36.dp).scaled(scale).coerceAtLeast(32.dp))
+                .repeatingClickable(
+                    onClick = onIncrement,
+                ),
             contentPadding = if (isLarge) ButtonDefaults.TextButtonContentPadding else PaddingValues(0.dp),
             colors = ButtonDefaults.textButtonColors(
                 contentColor = incrementColor
@@ -96,6 +118,37 @@ fun CounterDisplay(
                 fontSize = (if (isLarge) 26.sp else 16.sp).scaled(scale),
                 fontWeight = FontWeight.Bold
             )
+        }
+    }
+}
+
+fun Modifier.repeatingClickable(
+    maxDelayMillis: Long = 750,
+    minDelayMillis: Long = 100,
+    delayDecayFactor: Float = .20f,
+    onClick: () -> Unit,
+): Modifier = composed {
+    val currentClickListener by rememberUpdatedState(onClick)
+
+    pointerInput(Unit) {
+        coroutineScope {
+            awaitEachGesture {
+                val down = awaitFirstDown(requireUnconsumed = false)
+
+                val job = launch {
+                    var currentDelayMillis = maxDelayMillis
+
+                    while (down.pressed) {
+                        currentClickListener()
+                        delay(currentDelayMillis.milliseconds)
+                        val nextMillis = currentDelayMillis - (currentDelayMillis * delayDecayFactor)
+                        currentDelayMillis = nextMillis.toLong().coerceAtLeast(minDelayMillis)
+                    }
+                }
+
+                waitForUpOrCancellation()
+                job.cancel()
+            }
         }
     }
 }
