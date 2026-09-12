db8014a84685b53d2f1800a10c61bd7e6929c76a
GaroLuis <garoluis94@gmail.com>
2026-09-12 14:58:00 +0200

    feat: add 1.1.2 apk

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,7 +16,7 @@ android {
         minSdk = 26
         targetSdk = 37
         versionCode = 1
-        versionName = "1.1.1"
+        versionName = "1.1.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
